da74a60681525f0d4f81bf1c7a0b6e11b3d90bce
Pierre <pierre.malherbe6@gmail.com>
2018-11-10 21:47:40 +0100

    Add new fields Bottle Entity

--- src/main/java/com/malherbe/cocktailcore/resource/model/entity/BottleEntity.java
+++ src/main/java/com/malherbe/cocktailcore/resource/model/entity/BottleEntity.java
@@ -13,22 +13,72 @@ public class BottleEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firstname")
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "picture")
+    private String picture;
+
+    @Column(name = "capacity")
+    private Double capacity;
+
+    @Column(name = "remaining_capacity")
+    private Double remainingCapacity;
+
+    @Column(name = "pin")
+    private Integer pin;
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public BottleEntity setId(Long id) {
         this.id = id;
+        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public BottleEntity setName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public BottleEntity setPicture(String picture) {
+        this.picture = picture;
+        return this;
+    }
+
+    public Double getCapacity() {
+        return capacity;
+    }
+
+    public BottleEntity setCapacity(Double capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+
+    public Double getRemainingCapacity() {
+        return remainingCapacity;
+    }
+
+    public BottleEntity setRemainingCapacity(Double remainingCapacity) {
+        this.remainingCapacity = remainingCapacity;
+        return this;
+    }
+
+    public Integer getPin() {
+        return pin;
+    }
+
+    public BottleEntity setPin(Integer pin) {
+        this.pin = pin;
+        return this;
     }
 }
